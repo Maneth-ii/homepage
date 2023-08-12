814068efa0fd22274714846dd185b3b8fc06302d
Maneth-ii <manethbandara25@gmail.com>
2023-08-12 16:19:04 +0530

    fix

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
+const searchInputFieldValue = document.getElementById("search-el").value
+
 const links = [
   {
     id:1,
     name:"Youtube",
+    color:"red",
     href:"https://www.youtube.com"
   },
   {
     id:2,
     name:"Facebook",
+    color:"blue",
     href:"https://www.facebook.com"
   },
   {
     id:3,
     name:"ChatGPT",
+    color:"green",
     href:"https://chat.openai.com/"
   },
   {
     id:4,
     name:"Github",
+    color:"black",
     href:"https://www.github.com"
   },
   {
@@ -34,13 +40,18 @@ const links = [
     name:"Google",
     href:"https://www.google.com"
   },
+  {
+    id:7,
+    name:"Google",
+    href:"https://www.google.com"
+  },
   
 ];
 
 const linksList = links.map((aLink) =>{
   return(
-    `<li class="list-item">
-      <a class="link" href=${aLink.href} target="_blank">
+    `<li class="list-item" style=background-color:${aLink.color};>
+      <a class="link" href=${aLink.href} target="_self">
         <div style="">
           <h1>${aLink.name}</h1>
         </div>
